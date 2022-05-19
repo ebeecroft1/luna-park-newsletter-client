9c851759c0f12ccc7fb6ad37cf362a4f3fbd4db6
123mandy <xuetingsun1213@gmail.com>
2022-05-19 17:34:54 +1000

    basic setting done

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Signup from "../views/Signup.vue";
+import Success from "../views/Success.vue";
+import Users from "../views/Users.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
-const router = new VueRouter({
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  linkActiveClass: "active",
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    },
+    { 
+      path: '/users',
+      name: 'users',
+      component: Users
+    },
+    { 
+      path: '/signup/success',
+      name: 'success',
+      component: Success
+    }
+  ]
+});
 
-export default router
