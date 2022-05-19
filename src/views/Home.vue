2f3f08daf26affd231c19c8bb9cbb2a63c4537d3
ebeecroft1 <eric.beecroft@gmail.com>
2022-05-19 22:53:14 +1000

    homepage work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="tickets">
+      <h3>GET MORE AIR TIME!</h3>
+      <p>BOOK EARLY AND SAVE!</p>
+      <b-button pill variant="danger">Buy Tickets</b-button>
+    </div>
+
+    <img src="../assets/gate.png" alt="Family at Luna Park Gates" class="img-banner">
+
+    <div class="signup">
+      <h4>JOIN IN THE FUN</h4>
+      <p>Get the latest news, special deals and exclusive offers delivered straight to your inbox.</p>
+
+      <b-button pill class="px-5 m-3" variant="danger" href="/signup">Sign Up</b-button>
+
+      <p>Join 42,000+ thrillseekers. Unsubscribe anytime.</p>
+    </div>
 
   </div>
 </template>
@@ -8,3 +23,25 @@
 <script>
 
 </script>
+
+<style>
+  .tickets {
+    height: 244px;
+    background-image: url("~@/assets/get-air.png");
+    background-repeat: no-repeat;
+    background-size: 100%;
+    color: #FFFFFF;
+  }
+
+  .img-banner{
+    width: 100%;
+  }
+
+  .signup {
+    background-color: #094268;
+    height: 226px;
+    width: 100%;
+    color: #FFFFFF;
+  }
+
+</style>
